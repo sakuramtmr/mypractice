1d787c5e87b4e7985108141b045e20623ec1b707
“SAKUmtmr” <saku.yeah@gmail.com>
2017-11-05 18:39:22 +0900

    練習

--- test.js
+++ test.js
@@ -1,10 +1,2 @@
-var score = 20;
-
-	if (score > 60){
-		console.log("ok!");
-	}else if(score > 20){
-		console.log("soso");
-	}else{
-		console.log("ng!");
-	}
-	
+var s = new String("saku");
+document.write(s.length);
